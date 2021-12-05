d35ab3bcba180abb28ba5b81bec13f898d877fd4
StanDeMan <StanislausE@gmx.de>
2021-12-05 20:23:41 +0100

    Move Startup to .NET 6

--- Domotica.Core/Startup.cs
+++ Domotica.Core/Startup.cs
@@ -23,7 +23,6 @@ namespace Domotica.Core
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        [System.Obsolete]
         public void Configure(IApplicationBuilder app, IHostEnvironment env)
         {
             if (env.IsDevelopment())
