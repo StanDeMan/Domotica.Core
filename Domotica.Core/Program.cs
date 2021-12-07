e369a883c192617695e478d8d35fab8edf808851
StanDeMan <StanislausE@gmx.de>
2021-12-07 22:33:20 +0100

    Move to new coding style (.NET 6)

--- Domotica.Core/Program.cs
+++ Domotica.Core/Program.cs
@@ -1,23 +1,34 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Domotica.Core.Hubs;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Domotica.Core
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args)
-                .Build()
-                .Run();
-        }
+var builder = WebApplication.CreateBuilder(args);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host
-            .CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
-    }
+// Add services to the container.
+builder.Services.AddMvc(option => option.EnableEndpointRouting = false);
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+
+app.UseExceptionHandler("/Error");
+app.UseHsts();
+app.UseHttpsRedirection();
+app.UseFileServer();
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapHub<Device>("/Hubs/Device");
+    endpoints.MapHub<Hello>("/Hubs/Hello");
+});
+app.UseMvc();
+app.UseMvcWithDefaultRoute();
+app.UseHsts();
+
+app.Run();
