cbe9dc5d6952bf5615e28576d25d20a10572e1e8
StanDeMan <StanislausE@gmx.de>
2021-12-25 15:57:36 +0100

    Unittests for identity token

---
+++ Test.Domotica.Core/UnitTestidentity.cs
@@ -0,0 +1,17 @@
+﻿using Domotica.Core.Identity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Domotica.Core
+{
+    [TestClass]
+    public class UnitTestIdentity
+    {
+        [TestMethod]
+        public void TestReadToken()
+        {
+            var token = new Token().Generate();
+
+            Assert.IsNotNull(token);    
+        }
+    }
+}
