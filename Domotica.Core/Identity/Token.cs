f0a49f5bbfd8668d20a994ca77812b0e873a47f8
StanDeMan <StanislausE@gmx.de>
2021-12-25 15:57:19 +0100

    Implement identity token

---
+++ Domotica.Core/Identity/Token.cs
@@ -0,0 +1,24 @@
+﻿using shortid;
+using shortid.Configuration;
+
+namespace Domotica.Core.Identity;
+
+public class Token
+{
+    public int Seed { get; } = 19641108;    // take birthday as seed 
+
+    public Token(int? seed = null)
+    {
+        ShortId.SetSeed(seed ?? Seed);
+    }
+
+    public string Generate()
+    {
+        return ShortId.Generate(new GenerationOptions
+        {
+            Length = 12,
+            UseNumbers = true,
+            UseSpecialCharacters = false
+        });
+    }
+}
