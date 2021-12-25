701a73327e5d083e63c179594990af1f178f10c3
StanDeMan <StanislausE@gmx.de>
2021-12-25 14:23:08 +0100

    Test for Network MAC address

---
+++ Test.Domotica.Core/UnitTest1.cs
@@ -0,0 +1,36 @@
+using System.Diagnostics;
+using Domotica.Core.Hardware;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Domotica.Core
+{
+    [TestClass]
+    public class UnitTestNetwork
+    {
+        [TestMethod]
+        public void TestReadMacAddress()
+        {
+            var network = new Mac();
+            var mac = network.ReadAddress();
+
+            Assert.IsNotNull(mac);
+            Debug.Print($"Read MAC address: {mac}");
+        }
+
+        [TestMethod]
+        public void TestReadMacAddresses()
+        {
+            var network = new Mac();
+            var networks = network.ReadAddresses();
+
+            Assert.AreNotEqual(0, networks.Count);
+           
+            Debug.Print($"MAC addresses found: {networks.Count}");
+
+            foreach (var net in networks)
+            {
+                Debug.Print($"MAC address: {net.GetPhysicalAddress()}");
+            }
+        }
+    }
+}
