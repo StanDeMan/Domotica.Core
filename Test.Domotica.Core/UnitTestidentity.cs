493b6a5cb4caa32346945c075f58460c21ab8dbf
StanDeMan <StanislausE@gmx.de>
2021-12-25 16:21:46 +0100

    Test length token handling

--- Test.Domotica.Core/UnitTestidentity.cs
+++ Test.Domotica.Core/UnitTestidentity.cs
@@ -1,4 +1,6 @@
-﻿using Domotica.Core.Identity;
+﻿using System;
+using System.Diagnostics;
+using Domotica.Core.Identity;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Test.Domotica.Core
@@ -22,6 +24,43 @@ namespace Test.Domotica.Core
 
             Assert.IsNotNull(token);  
             Assert.AreEqual(12, token.Length);  // default: set to 12 chars
+        }
+
+        [TestMethod]
+        public void TestGenerateWithDigLength()
+        {
+            var token = new Token().Generate(8);
+
+            Assert.IsNotNull(token);  
+            Assert.AreEqual(8, token.Length);  // set to 8 chars
+        }
+
+        [TestMethod]
+        public void TestGenerateWithArgumentTooLowException()
+        {
+            try
+            {
+                new Token().Generate(7);
+            }
+            catch (ArgumentException e)
+            {
+                Debug.WriteLine(e);
+                Assert.AreNotEqual(null, e);
+            }
+        }
+
+        [TestMethod]
+        public void TestGenerateWithArgumentTooHighException()
+        {
+            try
+            {
+                new Token().Generate(13);
+            }
+            catch (ArgumentException e)
+            {
+                Debug.WriteLine(e);
+                Assert.AreNotEqual(null, e);
+            }
         }
     }
 }
