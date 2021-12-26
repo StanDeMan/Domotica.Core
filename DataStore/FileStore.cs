539e887c16b6ff25991b2d497352a3237c2b4002
StanDeMan <StanislausE@gmx.de>
2021-12-26 03:53:18 +0100

    DataStore Implementation with first incomplete test

---
+++ DataStore/FileStore.cs
@@ -0,0 +1,120 @@
+﻿using Newtonsoft.Json.Linq;
+using JsonFlatFileDataStore;
+using Serilog;
+
+namespace DataBase
+{
+    public class FileStore : IDisposable
+    {
+        private const string DbName = "database";
+        private const string CollectionName = "devices";
+
+        public bool IsRunning { get; set; }
+        public string? DataBaseName { get; } = DbName;
+
+        private readonly DataStore? _store;
+
+        public FileStore(string? dataBaseName = null)
+        {
+            try
+            {
+                // Generate store with upper camel case json
+                _store = dataBaseName == null 
+                    ? new DataStore($"{DataBaseName}.json", false) 
+                    : new DataStore($"{DbName}.json", false);
+
+                DataBaseName = dataBaseName ?? DataBaseName;
+
+                Log.Logger = new LoggerConfiguration()
+                    .MinimumLevel.Warning()
+                    .WriteTo.File(@$"logs\{DataBaseName}.log", rollingInterval: RollingInterval.Day)
+                    .CreateLogger();
+
+                IsRunning = true;
+            }
+            catch (Exception e)
+            {
+                Log.Error($"DataBase.File initializing: {e}");
+
+                IsRunning = false;
+            }
+        }
+
+        public async Task<bool> Insert(string json)
+        {
+            try
+            {
+                var deviceJson = JToken.Parse(json);
+
+                var collection = _store?.GetCollection(CollectionName);
+
+                var ok = await collection?.InsertOneAsync(deviceJson)!;
+
+                return ok;
+            }
+            catch (Exception e)
+            {
+                Log.Error($"DataBase.File.Insert: {e}");
+                
+                return false;
+            }
+        }
+
+        public async Task<(bool, dynamic)> Read(int id)
+        {
+            try
+            {
+                var collection = _store?.GetCollection(CollectionName);
+
+                var result =  await Task.Run(() => collection?.AsQueryable().FirstOrDefault(e => e.Id == id)!);
+
+                return (true, result);
+            }
+            catch (Exception e)
+            {
+                Log.Error($"DataBase.File.Read: {e}");
+
+                return (false, string.Empty);
+            }
+        }
+
+        public async Task<bool> Update(int id, string json)
+        {
+            try
+            {
+                var deviceJson = JToken.Parse(json);
+
+                var collection = _store?.GetCollection(CollectionName);
+
+                return await collection?.UpdateOneAsync(e => e.Id == id, deviceJson)!;
+            }
+            catch (Exception e)
+            {
+                Log.Error($"DataBase.File.Update: {e}");
+                
+                return false;
+            }
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            try
+            {
+                var collection = _store?.GetCollection(CollectionName);
+                
+                return await collection?.DeleteOneAsync(e => e.Id == id)!;
+            }
+            catch (Exception e)
+            {
+                Log.Error($"DataBase.File.Delete: {e}");
+                
+                return false;
+            }
+        }
+
+        public void Dispose()
+        {
+            _store?.Dispose();
+        }
+    }
+}
---
+++ Test.Domotica.Core/UnitTestDataStore.cs
@@ -0,0 +1,51 @@
+﻿using DataBase;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Test.Domotica.Core
+{
+    [TestClass]
+    public class UnitTestDataStore
+    {
+        [TestMethod]
+        public async Task TestDataStore()
+        {
+            using var store = new FileStore();
+
+            if (!store.IsRunning)
+            {
+                Assert.Fail("FileStore not running!");
+            }
+
+            const string json = 
+                @"{
+                    'Id': 0,
+                    'Name': 'AmbientLigth',
+                    'LedRGBStripe': {
+                        'Brightness': 0,
+                        'LastValue': 100,
+                        'Color': {
+                            'R': 0,
+                            'G': 0,
+                            'B': 0
+                        }
+                    }
+                }";
+
+
+            var ok = await store.Insert(json);
+            Assert.IsTrue(ok);
+
+            var (okRead, readJson) = await store.Read(0);
+            Assert.IsTrue(okRead);
+
+            var storedJson = JsonConvert.SerializeObject(readJson);
+            var oldJson = JsonConvert.SerializeObject(JToken.Parse(json));
+
+            var equal = JToken.DeepEquals(storedJson, oldJson);
+            Assert.IsTrue(equal);
+        }
+    }
+}
