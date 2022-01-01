834fe7e9d208fffbf3e9688157849a3f0ac237e2
StanDeMan <StanislausE@gmx.de>
2022-01-01 21:14:07 +0100

    Remove unused method

--- Domotica.Core/Functionality/ExtendAssembly.cs
+++ Domotica.Core/Functionality/ExtendAssembly.cs
@@ -71,24 +71,5 @@ namespace Domotica.Core.Functionality
 
             return methodInfo.Invoke(_classInstance, methodParams);
         }
-
-        public object? ExecuteStatic(string methodName)
-        {
-            var methodInfoStatic = Type?.GetMethod(methodName);
-            
-            if (methodInfoStatic == null)
-            {
-                // never throw generic Exception - replace this with some other exception type
-                throw new Exception("No such static method exists.");
-            }
-
-            // Specify parameters for static method: 'public static void MyMethod(int count, float radius)'
-            var staticParameters = new object[2];
-            staticParameters[0] = 10;
-            staticParameters[1] = 3.14159f;
-
-            // Invoke static method
-            return methodInfoStatic.Invoke(_classInstance, staticParameters);
-        }
     }
 }
